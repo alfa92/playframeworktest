6d797a91878c661a406733cd3b35999d3b1e8af2
floriant <florian.torel@gmail.com>
2015-05-07 18:16:29 +0200

    Premier test
    Commit

--- test2/app/controllers/Application.java
+++ test2/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("ça marche"));
+        return ok(index.render("KK"));
     }
 
 }
